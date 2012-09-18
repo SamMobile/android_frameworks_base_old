CyanogenMod/android_frameworks_base

hwui: Disable framebuffer discarding for exynos4 devices
Change-Id: I27671f1dfc32f19dc792b9b13c328247e2b7b5fc

libs/hwui/Android.mk
@@ -39,6 +39,10 @@ ifeq ($(USE_OPENGL_RENDERER),true)
 		external/skia/src/ports \
 		external/skia/include/utils
 
+        ifeq ($(TARGET_PLATFORM),exynos4)
+            LOCAL_CFLAGS += -DDONT_DISCARD_FRAMEBUFFER
+        endif
+
 	LOCAL_CFLAGS += -DUSE_OPENGL_RENDERER -DGL_GLEXT_PROTOTYPES
 	LOCAL_CFLAGS += -fvisibility=hidden
 	LOCAL_MODULE_CLASS := SHARED_LIBRARIES

SamMobile/android_frameworks_base_old

libs/hwui/Android.mk
@@ -39,6 +39,10 @@ ifeq ($(USE_OPENGL_RENDERER),true)
 		external/skia/src/ports \
 		external/skia/include/utils
 
+ifeq ($(TARGET_BOARD_PLATFORM),exynos4)
+	LOCAL_CFLAGS += -DDONT_DISCARD_FRAMEBUFFER
+endif
+
 	LOCAL_CFLAGS += -DUSE_OPENGL_RENDERER -DGL_GLEXT_PROTOTYPES
 	LOCAL_CFLAGS += -fvisibility=hidden
 	LOCAL_MODULE_CLASS := SHARED_LIBRARIES